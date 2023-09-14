3509e4ac29d16cd15880675d38b8efa5bae1b473
Tushar Kalbhande <tusharkalbhande18@gmail.com>
2023-09-14 13:59:53 +0530

    Firebase analytics added

---
+++ src/constants/firebaseConfig.js
@@ -0,0 +1,6 @@
+import * as firebase from "firebase";
+
+
+
+// Initialize Firebase
+// export const firebaseAnalytics = firebase.analytics();
--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import * as firebase from "firebase/app";
+export const firebaseConfig = {
+  apiKey: "AIzaSyDgfOijkb-W4Qi4bem88V307mg1fgfF3cI",
+  authDomain: "tusharkalbhande-c84e5.firebaseapp.com",
+  databaseURL: "https://tusharkalbhande-c84e5-default-rtdb.firebaseio.com",
+  projectId: "tusharkalbhande-c84e5",
+  storageBucket: "tusharkalbhande-c84e5.appspot.com",
+  messagingSenderId: "998930722516",
+  appId: "1:998930722516:web:5989a3c7a68d498f779541",
+  measurementId: "G-63KJ8DKZD7",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
